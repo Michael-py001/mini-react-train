b29a25d6cf384451fbb6e66cb3427c74a295115a
Michael <969149642@qq.com>
2024-01-14 19:30:04 +0800

    实现render方法:动态渲染dom节点

--- day01/main.js
+++ day01/main.js
@@ -1,4 +1,4 @@
-// v1 
+// v1
 // const dom = document.createElement("div");
 // dom.id = "root"
 // document.querySelector('#root').append(dom)
@@ -34,7 +34,7 @@ function createTextNode(text) {
       nodeValue: text,
       children: [],
     },
-  }
+  };
 }
 
 //5 封装createElement
@@ -43,18 +43,50 @@ function createElement(type, props, ...children) {
     type,
     props: {
       ...props,
-      children
-    }
-  }
+      children: children.map((child) =>{
+        return typeof child === "object" ? child : createTextNode(child)
+      }),
+    },
+  };
 }
-const textEl = createTextNode('hello world')
-const App = createElement('div', {id: 'root'}, textEl)
+
+// // 6 使用封装的方法动态生成虚拟dom
+// const textEl = createTextNode("hello world");
+// const App = createElement("div", { id: "root" }, textEl);
 
 // 3 根据js描述的节点对象创建真实dom vdom -> dom
-const dom = document.createElement(App.type);
-dom.id = App.props.id;
-document.querySelector('#root').append(dom)
+// const dom = document.createElement(App.type);
+// dom.id = App.props.id;
+// document.querySelector("#root").append(dom);
 
-const textNode = document.createTextNode("");
-textNode.nodeValue = textEl.props.nodeValue
-dom.append(textNode)
+// const textNode = document.createTextNode("");
+// textNode.nodeValue = textEl.props.nodeValue;
+// dom.append(textNode);
+
+// 7 封装render方法
+
+function render(el,container) {
+  const dom =
+    el.type === "TEXT_ELEMENT"
+      ? document.createTextNode(el.props.nodeValue)
+      : document.createElement(el.type);
+  // 设置props
+  Object.keys(el.props).forEach((key) => {
+    if(key !== 'children') { //children特殊处理
+      dom[key] = el.props[key]
+    }
+  })
+
+  // 处理children节点
+  const chilren = el.props.children || [];
+  chilren.forEach(child=>{
+    render(child,dom)
+  })
+
+  container.append(dom)
+}
+
+const App = createElement("div", { id: "root" }, 'hi ','hello world');
+// const App = createElement("div", { id: "root" }, 'hello world');
+console.log("App:",App);
+render(App,document.querySelector('#root'))
